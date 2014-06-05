e1e69bdb60137f458e6b5cb0a3ab83a8fd894f6f
davidgertmenianwong <david@giftrocket.com>
2014-06-05 15:53:25 -0700

    Start pseudoclassical queue with protypal to illustrate differences

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,28 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var _someInstance = Object.create(queueMethods)
+  _someInstance._storage = {};
+  _someInstance._size = 0;
+  _someInstance._first = 0;
+
+  return _someInstance;
 };
+
+var queueMethods = {};
+queueMethods.enqueue = function(value) {
+  this._storage[this._size + this._first] = value;
+  this._size++;
+};
+queueMethods.dequeue = function(){
+  this._size > 0 && this._size--;
+
+  var temp = this._storage[this._first];
+  delete this._storage[this._first];
+  this._first++;
+  return temp;
+};
+queueMethods.size = function() {
+  return this._size;
+};
+
 
 
