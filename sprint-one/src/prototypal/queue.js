1af74c7cc56bf7dd5d2679f9098b67b99195d2f3
davidgertmenianwong <david@giftrocket.com>
2014-06-05 15:02:36 -0700

    Start protypal with functional-shared to illustrate differences

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,36 @@
-var makeQueue = function() {
+var makeQueue = function(){
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var _someInstance = mixinFunc({});
+  _someInstance._storage = {};
+  _someInstance._size = 0;
+  _someInstance._first = 0;
+
+  return _someInstance;
 };
 
 var queueMethods = {};
+queueMethods.enqueue = function(value) {
+  this._storage[this._size + this._first] = value;
+  this._size++;
+};
+queueMethods.dequeue = function(){
+  this._size > 0 && this._size--;
+
+  var temp = this._storage[this._first];
+  delete this._storage[this._first];
+  this._first++;
+  return temp;
+};
+queueMethods.size = function() {
+  return this._size;
+};
+
+var mixinFunc = function(result){
+  result.enqueue = queueMethods.enqueue;
+  result.dequeue = queueMethods.dequeue;
+  result.size = queueMethods.size;
+  return result;
+}
 
 
