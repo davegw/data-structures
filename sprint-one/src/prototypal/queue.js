03722a1db053240d31bd42b4a273625fd7bdc98c
davidgertmenianwong <david@giftrocket.com>
2014-06-05 15:14:37 -0700

    Complete protypal pattern for queue

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,7 +1,7 @@
 var makeQueue = function(){
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  var _someInstance = mixinFunc({});
+  var _someInstance = Object.create(queueMethods)
   _someInstance._storage = {};
   _someInstance._size = 0;
   _someInstance._first = 0;
@@ -25,12 +25,5 @@ queueMethods.dequeue = function(){
 queueMethods.size = function() {
   return this._size;
 };
-
-var mixinFunc = function(result){
-  result.enqueue = queueMethods.enqueue;
-  result.dequeue = queueMethods.dequeue;
-  result.size = queueMethods.size;
-  return result;
-}
 
 
