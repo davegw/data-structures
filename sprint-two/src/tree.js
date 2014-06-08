084b3bbe349330210612aa61f69de8bcdbcabf7c
Hack Reactor Students <dgertmenianwong@gmail.com>
2014-06-07 18:21:30 -0700

    Complete traverse for tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -35,6 +35,16 @@ treeMethods.contains = function(target){
   return wasFound;
 };
 
+treeMethods.traverse = function(callback){
+  var traverseTree = function(tree) {
+    callback(tree.value);
+    _.each(tree.children, function(child) {
+      traverseTree(child);
+    });
+  };
+  traverseTree(this);
+};
+
 
 /*
  * Complexity: What is the time complexity of the above functions?
